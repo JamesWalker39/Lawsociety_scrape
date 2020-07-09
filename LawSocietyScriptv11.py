bdff63b57f9259e07ccaccec7b6d6bef7ecfea50
James Walker <walker.james39@hotmail.co.uk>
2020-07-09 11:05:43 +0100

    picks up COFA

--- LawSocietyScriptv11.py
+++ LawSocietyScriptv11.py
@@ -18,9 +18,10 @@ def urlreader_to_soup(url):
 location = "bristol"
 
 # Creating CSV for details to be written too 
-filename = "SolicitorsDetails.csv"
+filename = "Solicitors_Details_"+location+".csv"
 f = open(filename , "w")
 headers = "Company_Name,Address,Telephone,Num_Offices,Num_Solicitors,Num_Accreditations,COFA\n" 
+f.write("Solicitors: " + location + "\n")
 f.write(headers)
 
 # get page count
@@ -68,32 +69,40 @@ for pagenumber in range (1,pages):
         print("opened: " + co_page + "\n")
         co_page_data = urlreader_to_soup(co_page)
         table_rows = co_page_data.findAll("div", {"class":"panel-third"})
-        SRA_link = "https://solicitors.lawsociety.org.uk"+ table_rows[0].findAll("a")[2].get('href')
+        try:
+            SRA_link = "https://solicitors.lawsociety.org.uk"+ table_rows[1].findAll("a")[2].get('href')
+        except:
+            break
                     
-        '''
-        Need to loop through SRA pages
-        '''
-        
         #open SRA page 
         SRA_page_data = urlreader_to_soup(SRA_link)
         print("opened: " + SRA_link + "\n")
+        No_SRA = SRA_page_data.find("h1").strong.text
+        SRA_pages = (int(No_SRA)//20) + 2
+        COFA = ""
+        
+        for SRApagenumber in range (1,SRA_pages):
+            SRA_page_data_loop = "https://solicitors.lawsociety.org.uk"+ table_rows[1].findAll("a")[2].get('href')+"&Page="+ str(SRApagenumber)
+            print("opened: " + SRA_page_data_loop + "\n")
+            SRA_currentpage = urlreader_to_soup(SRA_page_data_loop)
         
         #find SRA data, loop, open page and check COFA - requests used as bytes
-        for SRA in SRA_page_data.findAll("h2"):
-            COFA_page_link = "https://solicitors.lawsociety.org.uk"+ SRA.a.get("href")
-            COFA_page = requests.get(str(COFA_page_link)).text
-            print()
-            COFA_page_data = soup(COFA_page,"html.parser")
-            try:
-                COFA_table = COFA_page_data.findAll("div", {"class":"panel-half"})[1].dd.ul.findAll("li")
-                COFA_found = False
-                for i in COFA_table:
-                    if i.text == "Compliance Officer for Finance and Administration":
-                        COFA_found = True
-                        COFA = COFA_page_data.h1.text
-                        break
-            except:
-                print("Non conforming page layout")
+            for SRA in SRA_currentpage.findAll("h2"):
+                COFA_page_link = "https://solicitors.lawsociety.org.uk"+ SRA.a.get("href")
+                print(COFA_page_link)
+                COFA_page = requests.get(str(COFA_page_link)).text
+                COFA_page_data = soup(COFA_page,"html.parser")
+                try:
+                    COFA_table = COFA_page_data.findAll("div", {"class":"panel-half"})[1].dd.ul.findAll("li")
+                    COFA_found = False
+                    for i in COFA_table:
+                        if i.text == "Compliance Officer for Finance and Administration":
+                            COFA_found = True
+                            COFA = COFA_page_data.h1.text
+                            break
+                except:
+                    print("Non conforming page layout")
+                print("Checking COFA: "+ COFA_page_data.h1.text + "\n")
 
                                                     
         # prints to check  
@@ -138,5 +147,4 @@ for pagenumber in range (1,pages):
     # write to csv
         f.write("'"+ name + "," + address.replace(",",".") + "," + "'" + print_tel + "," + officeno.strip() + "," + solicno.strip() + "," + noAccreds.strip() + "," + COFA + "\n")
         sleep(1)
-        COFA = ""
 f.close()
